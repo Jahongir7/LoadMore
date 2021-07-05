9d3f2f5cd423aa2e615700724225df8590b490e5
Jahongir7 <Jahongir95119@gmail.com>
2021-07-05 15:54:39 +0500

    package

--- src/App.js
+++ src/App.js
@@ -1,76 +1,29 @@
-/** @jsx jsx */
-import { css, jsx } from "@emotion/core";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import ReactMarkdown from "react-markdown/with-html";
 
 const App = () => {
-  const [value, setValue] = useState("");
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(3);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
-    <div className="App" css={styles}>
-      <h1>Markdown</h1>
-      <div className="markdownContainer">
-        <textarea onChange={(e) => setValue(e.target.value)}></textarea>
-        <ReactMarkdown className="output" source={value} escapeHtml={false} />
+    <div className="App">
+      <div className="container">
+        {items.map((item) => {
+          <div className="card">
+            <div className="id">
+              <span>{item.id}</span>
+            </div>
+            <p>{item.body}</p>
+          </div>;
+        })}
       </div>
     </div>
   );
 };
-
-const styles = css`
-  width: 100%;
-  min-height: 100vh;
-  background: #444;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
-  h1 {
-    font-size: 40px;
-    line-height: 1;
-    margin-bottom: 18px;
-  }
-  .markdownContainer {
-    width: 100%;
-    max-width: 1400px;
-    display: flex;
-    a{
-      color: red;
-    }
-    textarea,
-    .output {
-      width: 100%;
-      max-width: 50%;
-      padding: 20px;
-    }
-    textarea {
-      width: 100%;
-      resize: none;
-      border: none;
-      outline: none;
-    }
-    .output {
-      background: #2d4059;
-      height: 800px;
-      text-align: center;
-    }
-  }
-  @media (max-width: 980px) {
-    .markdownContainer {
-      flex-direction: column;
-      .output,
-      textarea {
-        max-width: 100%;
-        height: 400px;
-      }
-    }
-  }
-  @media (max-width: 1440px) {
-    .markdownContainer {
-      max-width: 90%;
-    }
-  }
-`;
 
 export default App;
